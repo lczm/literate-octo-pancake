37acea782a12da17d785e7dc5ef1a3bfaa2f9bcf
Zhu Zhanyan <program.nom@gmail.com>
2023-01-15 22:12:19 +0800

    wip: part 2 solution that is too slow

--- zhanyan/day16/solution.py
+++ zhanyan/day16/solution.py
@@ -33,10 +33,10 @@ class Valve:
 
 def optimize(
     valves: Dict[str, Valve],
-    current: str,
+    current: Tuple[str, str],
     on_valves: FrozenSet[str] = frozenset(),
     time_left: int = 30,
-    memo: Dict[Tuple[str, FrozenSet[str], int], int] = {},
+    memo: Dict[Tuple[Tuple[str, str], FrozenSet[str], int], int] = {},
 ) -> int:
     """Traverse the given valves to find optimal order to toggle valves & release the most pressure."""
     # base case: no more time or all valves on
@@ -47,32 +47,54 @@ def optimize(
     if memo_key in memo:
         return memo[memo_key]
 
-    # recursively explore opening & not opening the valve
     max_pressure = 0
-    for adjacent in valves[current].connected:
-        open_pressure = 0
-        # optimization: skip opening valves that have no flowrate
-        if current not in on_valves and valves[current].flowrate > 0:
-            # explore opening the valve. T-2 needed to open & travel to next valves
-            open_pressure = (
-                optimize(
-                    valves,
-                    adjacent,
-                    on_valves | frozenset((current,)),
-                    time_left - 2,
-                    memo,
-                )
-                # calculate pressure relief brought on by current valve.
-                # T-1 to account for time need to open valve
-                + valves[current].flowrate * (time_left - 1)
+    you, elephant  = current
+    for you_next in valves[you].connected:
+        for elephant_next in valves[elephant].connected:
+            # optimization: don't visit the same valve at the same time remainding
+            if you_next == elephant_next:
+                continue
+            # optimization: skip opening valves that have no flowrate or already open
+            you_options = (
+                [you, None] if you not in on_valves and valves[you].flowrate > 0 else [None]
+            )
+            elephant_options = (
+                [elephant, None] 
+                    if elephant not in on_valves and valves[elephant].flowrate > 0 
+                    else [None]
             )
 
-        max_pressure = max(
-            max_pressure,
-            open_pressure,
-            # explore leaving the valve untouched. T-1 needed to travel to next valve
-            optimize(valves, adjacent, on_valves, time_left - 1, memo),
-        )
+            # explore opening or closing valves
+            for you_open in you_options:
+                for elephant_open in elephant_options:
+                    # compute release from current valves
+                    newly_opened = []
+                    if you_open is not None:
+                        newly_opened.append(you_open)
+                    if elephant_open is not None:
+                        newly_opened.append(elephant_open)
+
+                    new_release = (
+                        sum([valves[v].flowrate for v in newly_opened]) * (time_left - 1)
+                    )
+
+                    # determine position of you & elephant in next minute
+                    # if you did not open a valve, you would have moved to the next valve
+                    next_valves = (
+                        you_next if you_open is None else you,
+                        elephant_next if elephant_open is None else elephant,
+                    )
+
+                    max_pressure = max(
+                        max_pressure,
+                        # recursively explore opening & not opening the valve
+                        new_release + optimize(
+                            valves=valves, 
+                            current=next_valves,
+                            on_valves=on_valves | frozenset(newly_opened), 
+                            time_left=time_left - 1
+                        ),
+                    )
 
     # cache result for future calls
     memo[memo_key] = max_pressure
@@ -88,4 +110,4 @@ if __name__ == "__main__":
         valves = {v.name: v for v in [Valve.parse(l) for l in f.readlines()]}
 
     # part 1
-    print(optimize(valves, "AA", time_left=30))
+    print(optimize(valves, ("AA", "AA"), time_left=26))
